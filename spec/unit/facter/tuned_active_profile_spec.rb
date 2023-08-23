78c3815b96ad4371d21c9cb0d5566fe043d3202d
Joshua Hoblitt <josh@hoblitt.com>
2023-08-23 15:27:25 -0700

    fix rubocop warnings

--- spec/unit/facter/tuned_active_profile_spec.rb
+++ spec/unit/facter/tuned_active_profile_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 require 'facter'
 require 'facter/tuned_active_profile'
 
-describe :tuned_active_profile, type: :fact do
+describe ':tuned_active_profile', type: :fact do
   subject(:fact) { Facter.fact(:tuned_active_profile) }
 
   before do
@@ -16,34 +16,29 @@ describe :tuned_active_profile, type: :fact do
   active_profile_output = 'Current active profile: balanced'
 
   context 'with no tuned-adm' do
-    before do
-      expect(Facter::Util::Resolution).to receive(:which).at_least(1).with('tuned-adm').and_return(nil)
-      expect(Facter::Core::Execution).not_to receive(:exec)
-    end
-
     it do
+      allow(Facter::Util::Resolution).to receive(:which).at_least(1).with('tuned-adm').and_return(nil)
+      allow(Facter::Core::Execution).to receive(:exec)
+      expect(Facter::Core::Execution).not_to have_received(:exec)
+
       expect(fact.value).to be_nil
     end
   end
 
   context 'tuned not running' do
-    before do
-      expect(Facter::Util::Resolution).to receive(:which).at_least(1).with('tuned-adm').and_return('tuned-adm')
-      expect(Facter::Core::Execution).to receive(:exec).with('tuned-adm active 2>/dev/null').and_return(not_running_output)
-    end
-
     it do
+      allow(Facter::Util::Resolution).to receive(:which).at_least(1).with('tuned-adm').and_return('tuned-adm')
+      allow(Facter::Core::Execution).to receive(:exec).with('tuned-adm active 2>/dev/null').and_return(not_running_output)
+
       expect(fact.value).to be_nil
     end
   end
 
   context 'tuned running' do
-    before do
-      expect(Facter::Util::Resolution).to receive(:which).at_least(1).with('tuned-adm').and_return('tuned-adm')
-      expect(Facter::Core::Execution).to receive(:exec).with('tuned-adm active 2>/dev/null').and_return(active_profile_output)
-    end
-
     it do
+      allow(Facter::Util::Resolution).to receive(:which).at_least(1).with('tuned-adm').and_return('tuned-adm')
+      allow(Facter::Core::Execution).to receive(:exec).with('tuned-adm active 2>/dev/null').and_return(active_profile_output)
+
       expect(fact.value).to eq('balanced')
     end
   end
--- spec/unit/facter/tuned_recommended_profile_spec.rb
+++ spec/unit/facter/tuned_recommended_profile_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 require 'facter'
 require 'facter/tuned_recommended_profile'
 
-describe :tuned_recommended_profile, type: :fact do
+describe ':tuned_recommended_profile', type: :fact do
   subject(:fact) { Facter.fact(:tuned_recommended_profile) }
 
   before do
@@ -15,34 +15,29 @@ describe :tuned_recommended_profile, type: :fact do
   recommend_output = 'virtual-guest'
 
   context 'with no tuned-adm' do
-    before do
-      expect(Facter::Util::Resolution).to receive(:which).at_least(1).with('tuned-adm').and_return(nil)
-      expect(Facter::Core::Execution).not_to receive(:exec)
-    end
-
     it do
+      allow(Facter::Util::Resolution).to receive(:which).at_least(1).with('tuned-adm').and_return(nil)
+      allow(Facter::Core::Execution).to receive(:exec)
+      expect(Facter::Core::Execution).not_to have_received(:exec)
+
       expect(fact.value).to be_nil
     end
   end
 
   context 'tuned recomend disabled' do
-    before do
-      expect(Facter::Util::Resolution).to receive(:which).at_least(1).with('tuned-adm').and_return('tuned-adm')
-      expect(Facter::Core::Execution).to receive(:exec).with('tuned-adm recommend 2>/dev/null').and_return('')
-    end
-
     it do
+      allow(Facter::Util::Resolution).to receive(:which).at_least(1).with('tuned-adm').and_return('tuned-adm')
+      allow(Facter::Core::Execution).to receive(:exec).with('tuned-adm recommend 2>/dev/null').and_return('')
+
       expect(fact.value).to be_nil
     end
   end
 
   context 'tuned running' do
-    before do
-      expect(Facter::Util::Resolution).to receive(:which).at_least(1).with('tuned-adm').and_return('tuned-adm')
-      expect(Facter::Core::Execution).to receive(:exec).with('tuned-adm recommend 2>/dev/null').and_return(recommend_output)
-    end
-
     it do
+      allow(Facter::Util::Resolution).to receive(:which).at_least(1).with('tuned-adm').and_return('tuned-adm')
+      allow(Facter::Core::Execution).to receive(:exec).with('tuned-adm recommend 2>/dev/null').and_return(recommend_output)
+
       expect(fact.value).to eq('virtual-guest')
     end
   end
