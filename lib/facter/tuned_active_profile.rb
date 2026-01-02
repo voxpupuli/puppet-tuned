75a094ab72733b976af77019a8813386b59cb9ac
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2026-01-02 14:36:29 +0100

    Run fact code only in setcode block
    The code outside of setcode is always ran when loading the fact. This
    meant the confine statement wasn't effective.

--- lib/facter/tuned_active_profile.rb
+++ lib/facter/tuned_active_profile.rb
@@ -4,14 +4,14 @@ Facter.add(:tuned_active_profile) do
   # https://puppet.com/docs/puppet/latest/fact_overview.html
   confine kernel: 'Linux'
 
-  retval = nil
-
-  if Facter::Util::Resolution.which('tuned-adm')
-    cmd = Facter::Core::Execution.exec('tuned-adm active 2>/dev/null')
-    retval = Regexp.last_match(1) if cmd && cmd =~ %r{^Current active profile: (.*)$}
-  end
-
   setcode do
+    retval = nil
+
+    if Facter::Util::Resolution.which('tuned-adm')
+      cmd = Facter::Core::Execution.exec('tuned-adm active 2>/dev/null')
+      retval = Regexp.last_match(1) if cmd && cmd =~ %r{^Current active profile: (.*)$}
+    end
+
     retval
   end
 end
--- lib/facter/tuned_recommended_profile.rb
+++ lib/facter/tuned_recommended_profile.rb
@@ -4,12 +4,14 @@ Facter.add(:tuned_recommended_profile) do
   # https://puppet.com/docs/puppet/latest/fact_overview.html
   confine kernel: 'Linux'
 
-  if Facter::Util::Resolution.which('tuned-adm')
-    retval = Facter::Core::Execution.exec('tuned-adm recommend 2>/dev/null')
-    retval = nil if retval == ''
-  end
-
   setcode do
+    retval = nil
+
+    if Facter::Util::Resolution.which('tuned-adm')
+      retval = Facter::Core::Execution.exec('tuned-adm recommend 2>/dev/null')
+      retval = nil if retval == ''
+    end
+
     retval
   end
 end
