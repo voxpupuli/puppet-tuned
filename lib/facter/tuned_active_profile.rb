cf1f0b0ca7995b44a2167759f27b83c3498da8f3
Mikhail Stolyarov <x3om6ak@gmail.com>
2026-09-15 10:41:37 +0300

    [facter] Fixup "Unexpected key passed" warning

--- lib/facter/tuned_active_profile.rb
+++ lib/facter/tuned_active_profile.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'logger'
+
 Facter.add(:tuned_active_profile) do
   # https://docs.openvoxproject.org/openfact/latest/
   confine kernel: 'Linux'
@@ -8,7 +10,7 @@ Facter.add(:tuned_active_profile) do
     retval = nil
 
     if Facter::Core::Execution.which('tuned-adm')
-      cmd = Facter::Core::Execution.execute('tuned-adm active', on_fail: nil, stderr: '/dev/null')
+      cmd = Facter::Core::Execution.execute('tuned-adm active', on_fail: nil, logger: Logger.new(File::NULL))
       retval = Regexp.last_match(1) if cmd && cmd =~ %r{^Current active profile: (.*)$}
     end
 
